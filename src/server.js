4008b4b3ca7b53440b9df67fbc461634df482908
Israel <israelmyname@gmail.com>
2020-10-09 22:12:02 -0500

    mas cosasa en el server.js

--- src/server.js
+++ src/server.js
@@ -1,3 +1,24 @@
-const app = require('express');
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const app = express();
 
-app.listen(3000, console.log("HELLO HELLO"));
+//Ejs template
+app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname,'views'));
+
+//mighty morgan
+app.use(morgan('dev'));
+
+//forms
+app.use(express.urlencoded({extended:false}));
+
+//Routes
+
+
+
+//Static files
+app.use(express.static(path.join(__dirname,'/public')));
+
+
+app.listen(3000, console.log("HELLO HELLO"));   
